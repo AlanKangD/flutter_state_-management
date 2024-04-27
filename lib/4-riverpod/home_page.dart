3e3147f1d4a2afd9d7cdcdccfb56c575ca6ae9cb
alan <kno12350@gmail.com>
2024-04-27 15:39:27 +0900

    riverpod 에 사용되는 기본 클래스 및 화면에서 ProviderScope 를 사용해야 riverpod 에 도달할 수 있다

--- lib/4-riverpod/home_page.dart
+++ lib/4-riverpod/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider_pectice/4-riverpod/cart.dart';
 import 'package:provider_pectice/4-riverpod/store.dart';
 import 'package:provider_pectice/common/bottom_bar.dart';
@@ -16,23 +17,25 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: currentIndex,
-        children: const [
-          /// Store
-          Store(),
-
-          /// Cart
-          Cart(),
-        ],
-      ),
-      bottomNavigationBar: BottomBar(
-        currentIndex: currentIndex,
-        cartTotal: "0",
-        onTap: (index) => setState(() {
-          currentIndex = index;
-        }),
+    return ProviderScope(
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: const [
+            /// Store
+            Store(),
+      
+            /// Cart
+            Cart(),
+          ],
+        ),
+        bottomNavigationBar: BottomBar(
+          currentIndex: currentIndex,
+          cartTotal: "0",
+          onTap: (index) => setState(() {
+            currentIndex = index;
+          }),
+        ),
       ),
     );
   }
